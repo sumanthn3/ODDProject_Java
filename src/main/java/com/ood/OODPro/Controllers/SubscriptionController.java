1f210da19caa44892ebc4796c16300372ae1963e
Satwikha <sblsatwikha@gmail.com>
2023-04-09 01:48:49 -0400

    updated newSubscription

--- src/main/java/com/ood/OODPro/Controllers/SubscriptionController.java
+++ src/main/java/com/ood/OODPro/Controllers/SubscriptionController.java
@@ -1,15 +1,10 @@
 package com.ood.OODPro.Controllers;
 
-import com.ood.OODPro.Models.UserEntity;
 import com.ood.OODPro.Models.UserSubscriptionsEntity;
 import com.ood.OODPro.Payload.Request.AddSubscription;
-import com.ood.OODPro.Payload.Request.SignupRequest;
 import com.ood.OODPro.Payload.Response.NewSubscriptionResponse;
-import com.ood.OODPro.Payload.Response.SignupResponse;
 import com.ood.OODPro.Payload.Response.SubscriptionResponse;
-import com.ood.OODPro.Payload.Response.UserResponse;
 import com.ood.OODPro.Utils.JwtTokenUtil;
-//import com.ood.OODPro.repository.UserSubscriptionsEntity;
 import com.ood.OODPro.repository.SubscriptionRespository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
@@ -22,7 +17,6 @@ import org.springframework.web.bind.annotation.RestController;
 import javax.servlet.http.HttpServletRequest;
 import java.util.ArrayList;
 import java.util.List;
-import java.util.Optional;
 
 import org.springframework.web.bind.annotation.*;
 import javax.validation.Valid;
@@ -35,7 +29,6 @@ public class SubscriptionController {
     AuthenticationManager authenticationManager;
     @Autowired(required = false)
     SubscriptionRespository subscriptionRespository;
-    ;
 
     @Autowired
     JwtTokenUtil jwtUtils;
@@ -71,17 +64,10 @@ public class SubscriptionController {
     public ResponseEntity<?> newSubscription(@Valid @RequestBody AddSubscription addSubscription) {
         System.out.println("triggered new subscription api");
 
-//        Authentication authentication = authenticationManager
-//                .authenticate(new UsernamePasswordAuthenticationToken(addSubscription.getEmailId(), addSubscription.getPassword()));
-//
-//        SecurityContextHolder.getContext().setAuthentication(authentication);
-
-//        System.out.println("triggered signup api with body: " + AddSubscriptionResponse.toString());
 
         // Create new user's subscription
         UserSubscriptionsEntity subscription = new UserSubscriptionsEntity();
         subscription.setEmailId(addSubscription.getEmailId());
-        subscription.setSubscriptionName(addSubscription.getSubscriptionName());
         subscription.setSubscriptionName(addSubscription.getSubscriptionName());
         subscription.setSubscriptionPrice(addSubscription.getSubscriptionPrice());
         subscription.setBillingCycle(addSubscription.getBillingCycle());
